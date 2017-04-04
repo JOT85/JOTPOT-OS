02cb0ec06e78d0b712df5b7beabf98dc3f8e5b3d
Jacob O'Toole <jacob.otoole1@gmail.com>
2017-04-04 05:47:39 +0100

    Fixed jit and make sutable to bo put into an asar

--- jit.js
+++ jit.js
@@ -4,16 +4,16 @@ console.log("Loading packager...") ;
 let asar = require("asar") ;
 let fs = require("fs") ;
 
-function package(in,out) {
+function package(inf,out) {
 	
-	console.log(`Packaging ${in}...`) ;
+	console.log(`Packaging ${inf}...`) ;
 	if (typeof out === "undefined") {
 		
-		out = `${in}.asar` ;
+		out = `${inf}.asar` ;
 		
 	}
 	
-	return new Promise(resolve=>asar.createPackage(`./${in}`,`../resources/${out}`,resolve)) ;
+	return new Promise(resolve=>asar.createPackage(`./${inf}`,`../resources/${out}`,resolve)) ;
 	
 }
 
@@ -33,6 +33,6 @@ function copy(file) {
 package("electron").then(_=>package("JPOS")).then(_=>package("scougall-chrome")).then(_=>package("files")).then(_=>package("text-edit")).then(_=>copy("settings.json")).then(_=>copy("apps.json")).then(_=>{
 	
 	console.log("Booting JOTPOT OS...") ;
-	require("child_process").spawnSync("JPOS") ;
+	require("child_process").spawnSync("../JPOS") ;
 	
 }) ;
